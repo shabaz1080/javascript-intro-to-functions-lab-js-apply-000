3d155980f98d998052fceba38cebd7de2dbbed7b
whimsical-glitch-7555 <shabaz8010@gmail.com>
2020-09-01 02:17:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ function logWhisper(string){
 function sayHiToGrandma(string){
   if(string.toUpperCase()){
     return "YES INDEED!"
-  }else if(string.toLowerCase()){
+  }if(string.toLowerCase()){
   return "I can't hear you!"
   }
 }
