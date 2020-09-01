595b05b6e6873c2251679918d43ba7d111053c9c
whimsical-glitch-7555 <shabaz8010@gmail.com>
2020-09-01 02:15:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,14 +14,11 @@ function logWhisper(string){
   console.log(string.toLowerCase())
 }
 
-function sayHiToGrandma(string){
-  if(string.toLowerCase()){
-    return "I can't hear you!"
-}
-}
+
 
 function sayHiToGrandma(string){
   if(string.toUpperCase()){
     return "YES INDEED!"
   }
+  return "I can't hear you!"
 }
